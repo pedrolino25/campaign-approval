1e144759af1009acffb1823dbc79e127369078c5
pedrolino25 <pedrolino25@gmail.com>
2026-02-11 20:28:42 +0000

    fix typecheck

--- api/src/lib/handlers/index.ts
+++ api/src/lib/handlers/index.ts
@@ -1,11 +1,13 @@
-import { AuthService,BearerTokenExtractor, JwtVerifier } from '../auth/index'
+import { ClientReviewerRepository, UserRepository } from '../../repositories'
+import { AuthService,BearerTokenExtractor, JwtVerifier, RBACService } from '../auth/index'
 import { ErrorService } from '../errors/index'
 import { ApiHandlerFactory } from './api-handler-factory'
 import { SqsHandlerFactory } from './sqs-handler-factory'
 
 const tokenExtractor = new BearerTokenExtractor()
 const tokenVerifier = new JwtVerifier()
-const authService = new AuthService(tokenExtractor, tokenVerifier)
+const rbacService = new RBACService(new UserRepository(), new ClientReviewerRepository())
+const authService = new AuthService(tokenExtractor, tokenVerifier, rbacService)
 const errorService = new ErrorService()
 const apiHandlerFactory = new ApiHandlerFactory(authService, errorService)
 const sqsHandlerFactory = new SqsHandlerFactory()
