37ce3e5d0bf6756be171c0cbe363c0a58b7e9297
pedrolino25 <pedrolino25@gmail.com>
2026-02-13 13:35:30 +0000

    remove topbar

--- api/src/handlers/documentation.ts
+++ api/src/handlers/documentation.ts
@@ -48,10 +48,17 @@ const handleApiDocs = async (): Promise<APIGatewayProxyResult> => {
           filter: true,
           showExtensions: true,
           showCommonExtensions: true,
-          tryItOutEnabled: true
+          tryItOutEnabled: true,
+          defaultModelsExpandDepth: -1,
+          defaultModelExpandDepth: -1
         });
       };
     </script>
+    <style>
+      .swagger-ui .topbar {
+        display: none !important;
+      }
+    </style>
   </body>
 </html>`
 
