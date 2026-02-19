05981fc9b403df10f88baf0427dfc4efdceb09bc
pedrolino25 <pedrolino25@gmail.com>
2026-02-19 22:12:35 +0000

    fix logo

--- client/src/components/landing-pages/navbar/navbar.tsx
+++ client/src/components/landing-pages/navbar/navbar.tsx
@@ -184,7 +184,7 @@ export function Navbar() {
                 src={Logo}
                 alt="Worklient"
                 width={130}
-                className="mt-[-9px]"
+                className="md:mt-[-9px]"
               />
             </a>
 
