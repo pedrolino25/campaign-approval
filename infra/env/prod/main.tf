8d5037e503a33140ab46494787ff5e28e1b8e108
pedrolino25 <pedrolino25@gmail.com>
2026-02-25 01:30:21 +0000

    fix

--- infra/env/prod/main.tf
+++ infra/env/prod/main.tf
@@ -177,11 +177,11 @@ module "domain_mapping" {
 module "waf" {
   source = "../../modules/waf"
 
-  environment            = var.environment
-  api_gateway_stage_arn  = module.api_gateway.api_stage_arn
-  auth_rate_limit        = var.waf_auth_rate_limit
-  activation_rate_limit  = var.waf_activation_rate_limit
-  log_retention_days     = var.waf_log_retention_days
+  environment           = var.environment
+  api_gateway_stage_arn = module.api_gateway.api_stage_arn
+  auth_rate_limit       = var.waf_auth_rate_limit
+  activation_rate_limit = var.waf_activation_rate_limit
+  log_retention_days    = var.waf_log_retention_days
 
   tags = var.resource_tags
 }
