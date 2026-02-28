b4f5ed7ef8cf434639044d9690eaf207c3c8f6fd
pedrolino25 <pedrolino25@gmail.com>
2026-02-28 02:45:23 +0000

    fix

--- infra/modules/domain_mapping/main.tf
+++ infra/modules/domain_mapping/main.tf
@@ -14,10 +14,8 @@ resource "aws_apigatewayv2_domain_name" "main" {
   tags = var.tags
 }
 
-# Step 2a: Commented out so Terraform stops managing the mapping; apply will remove it from AWS so stage v1 can be deleted.
-# Restore this block in step 2b after switching to stage $default (uncomment and apply again).
-# resource "aws_apigatewayv2_api_mapping" "main" {
-#   api_id      = var.api_id
-#   domain_name = aws_apigatewayv2_domain_name.main.id
-#   stage       = var.stage_name
-# }
+resource "aws_apigatewayv2_api_mapping" "main" {
+  api_id      = var.api_id
+  domain_name = aws_apigatewayv2_domain_name.main.id
+  stage       = var.stage_name
+}
